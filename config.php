df0802293aca4b2b77486465f6b54c7250f58d2d
Hugo van der Geest <55272066+HugovanderGeest@users.noreply.github.com>
2020-06-14 18:38:58 +0200

    mail controlle

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php 
+
+$config = [
+    // BB, hostname, database etc 
+], 
+'MAIL' => [
+    'SMTP_HOST' => 'debugmail.io',
+    'SMTP_USER' => 'hugohugoi123@gmail.com',
+    'SMTP_PASSWORD' => 'e25e0d00-ad68-11ea-8baf-4f1c118e6e00',
+    'SMTP_PORT' => '25'
+], 
+    // base url, root etc
