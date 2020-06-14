8b7b3479173ce84460c373cc9258b354d47bf4e9
Hugo van der Geest <Hugo.vdg@outlook.com>
2020-06-14 18:36:46 +0200

    Update model.php

--- private/includes/model.php
+++ private/includes/model.php
@@ -1,39 +1,42 @@
-<?php 
+<?php
 
 include 'dbConnect.php';
 
-function get_posts() {
+function get_posts()
+{
     $pdo = dbConnect();
     $query = 'SELECT user_name, user_game1, user_game2, user_game3, user_discord, user_steam, user_img FROM `users`';
-    $statement = $pdo->query( $query );
+    $statement = $pdo->query($query);
     return $statement;
 }
 
-function get_user() {
+function get_user()
+{
     $pdo = dbConnect();
-    $query = 'SELECT user_name FROM `users`';
-    $statement = $pdo->prepare( $query );
+    $query = 'SELECT user_name FROM `gebruiker`';
+    $statement = $pdo->prepare($query);
     $statement->execute();
-    
+
     $user_data = $statement->fetchAll();
 
     return json_encode(array_values($user_data));
-
 };
 
-function isLoggedIn() {
+function isLoggedIn()
+{
     global $_SESSION;
 
     if (isset($_SESSION['id'])) {
         return true;
-    } 
+    }
     return false;
 }
 
-function register($user_name, $user_email, $user_password, $user_game1, $user_game2, $user_game3, $user_discord, $user_steam, $user_img) {
+function register($user_name, $user_email, $user_password, $user_game1, $user_game2, $user_game3, $user_discord, $user_steam, $user_img)
+{
     $pdo = dbConnect();
     $data = [$user_name, $user_email, $user_password, $user_game1, $user_game2, $user_game3, $user_discord, $user_steam, $user_img];
-    $sql = "INSERT INTO users (user_name, user_email, user_password, user_game1, user_game2, user_game3, user_discord, user_steam, user_img) VALUES (?, ? ,?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO gebruiker (user_name, user_email, user_password, user_game1, user_game2, user_game3, user_discord, user_steam, user_img) VALUES (?, ? ,?, ?, ?, ?, ?, ?, ?)";
     $pdo->prepare($sql)->execute($data);
     header('Location: index.php?page=home');
 }
